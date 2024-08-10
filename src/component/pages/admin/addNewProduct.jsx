a8fe879677ffd446094d862e77be21d4e61e66c0
Alok Singh <aloksingh36757@gmail.com>
2024-08-10 23:57:36 +0530

    post request for new product send successfully

--- src/component/pages/admin/addNewProduct.jsx
+++ src/component/pages/admin/addNewProduct.jsx
@@ -4,6 +4,10 @@ import axios from "axios";
 function AddNewProduct() {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [productName, setProductName] = useState("");
+  const [productDesc, setProductDesc] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productImage, setProductImage] = useState("");
 
   useEffect(() => {
     const getCategories = async () => {
@@ -20,6 +24,29 @@ function AddNewProduct() {
   const handleCategoryChange = (e) => {
     setSelectedCategory(e.target.value);
   };
+  const handleSubmit = async (e) => { 
+    e.preventDefault();// prevent default form submission
+
+    const newProduct = {
+        title:productName,
+        desciption:productDesc,
+        price:productPrice,
+        image:productImage,
+        category:selectedCategory
+    };
+    try {
+        const response = await axios.post("'https://fakestoreapi.com/products", newProduct);
+        console.log("Product added:", response.data);
+        //  Form fields ko reset kar denge  after successful submission
+        setProductName("");
+        setProductDesc("");
+        setProductPrice("");
+        setProductImage("");
+        setSelectedCategory("");
+      } catch (err) {
+        console.error("Error in adding product:", err);
+      }
+};
 
   return (
     <div className="flex flex-col">
@@ -38,6 +65,8 @@ function AddNewProduct() {
               type="text"
               placeholder="Enter Product Name"
               className="border border-black rounded-md ml-6"
+              value={productName} // Bind to state
+              onChange={(e) => setProductName(e.target.value)}// update state on input change
             />
           </label>
         </div>
@@ -48,6 +77,8 @@ function AddNewProduct() {
               type="text"
               placeholder="Enter the Description"
               className="border border-black rounded-md ml-6"
+              value={productDesc}// bind the state
+              onChange={(e) => setProductDesc(e.target.value)}// update the description on input change
             />
           </label>
         </div>
@@ -58,6 +89,8 @@ function AddNewProduct() {
               type="number"
               placeholder="Enter the Price"
               className="border border-black rounded-md ml-7"
+              value={productPrice}
+              onChange={(e)=> setProductPrice(e.target.value)}
             />
           </label>
         </div>
@@ -68,6 +101,8 @@ function AddNewProduct() {
               type="url"
               placeholder="Enter the Product Url"
               className="border border-black rounded-md ml-7"
+              value={productImage}
+              onChange={(e)=>setProductImage(e.target.value)}
             />
           </label>
         </div>
